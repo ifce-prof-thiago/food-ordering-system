6b0a66a445dab224e0d335a433d0fb51504d04db
Thiago Bandeira <thiago@lar.ifce.edu.br>
2023-07-08 14:05:23 -0300

    chore(java): update for graalvm 20

---
+++ order-service/order-domain/src/test/kotlin/food/ordering/system/order/domain/entities/CustomerTest.kt
@@ -0,0 +1,18 @@
+package food.ordering.system.order.domain.entities
+
+import food.ordering.system.common.domain.valueobjects.CustomerId
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class CustomerTest {
+
+    @Test
+    fun `should create customer`() {
+
+        val customerId = CustomerId(UUID.randomUUID())
+        val customer = Customer(CustomerProperties(customerId))
+
+        assertEquals(customerId, customer.id)
+    }
+}
